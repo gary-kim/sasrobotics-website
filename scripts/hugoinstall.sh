f80e3b95d7bbeb0a4362fc64824e8495253c732d
Gary Kim <gary@garykim.dev>
2019-06-16 19:38:05 +0800

    build: verify dependencies before installing

--- scripts/hugoinstall.sh
+++ scripts/hugoinstall.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+set -e
+
+SHA256SUM="afab8eaf12a9d72a072288f4ec5012cd889374cd56a855639b6ee1ba1686d174  hugo_0.55.6_Linux-64bit.deb"
+
 export CWD=`pwd`
 cd /tmp
-curl --output gohugo.deb -L "https://github.com/gohugoio/hugo/releases/download/v0.55.6/hugo_0.55.6_Linux-64bit.deb"
-sudo apt install -y ./gohugo.deb
+curl --output hugo_0.55.6_Linux-64bit.deb -L "https://github.com/gohugoio/hugo/releases/download/v0.55.6/hugo_0.55.6_Linux-64bit.deb"
+echo $SHA256SUM | sha256sum -c || exit 1
+sudo apt-get install -y ./hugo_0.55.6_Linux-64bit.deb
 cd $CWD
--- scripts/rcloneinstall.sh
+++ scripts/rcloneinstall.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+set -e
+
+SHA256SUM="e4b7a63ca6fa2bee37cbb33afeaa3fa60be2c595ef95a0b262ad8c8e770ea3b3  rclone-v1.48.0-linux-amd64.deb"
+
 export CWD=`pwd`
 cd /tmp
-curl --output rclone.deb -L "https://downloads.rclone.org/rclone-current-linux-amd64.deb"
-sudo apt install -y ./rclone.deb
+curl --output rclone-v1.48.0-linux-amd64.deb -L "https://github.com/ncw/rclone/releases/download/v1.48.0/rclone-v1.48.0-linux-amd64.deb"
+echo $SHA256SUM | sha256sum -c || exit 1
+sudo apt install -y ./rclone-v1.48.0-linux-amd64.deb
 cd $CWD
